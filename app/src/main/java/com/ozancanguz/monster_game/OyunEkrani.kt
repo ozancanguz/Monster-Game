5ed6da5b498fe8630980c7d022ad087a44c4046b
Ozan <ozancangu24@gmail.com>
2023-02-17 18:12:25 +0300

    -navigation created

--- app/src/main/java/com/ozancanguz/monster_game/OyunEkrani.kt
+++ app/src/main/java/com/ozancanguz/monster_game/OyunEkrani.kt
@@ -1,13 +1,20 @@
 package com.ozancanguz.monster_game
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_oyun_ekrani.*
 
 class OyunEkrani : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oyun_ekrani)
 
+        anakarakter.setOnClickListener {
+            startActivity(Intent(this@OyunEkrani,SonucEkraniActivity::class.java))
+
+            finish()
+        }
 
 
 
--- app/src/main/java/com/ozancanguz/monster_game/SonucEkraniActivity.kt
+++ app/src/main/java/com/ozancanguz/monster_game/SonucEkraniActivity.kt
@@ -1,11 +1,19 @@
 package com.ozancanguz.monster_game
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sonuc_ekrani.*
 
 class SonucEkraniActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sonuc_ekrani)
+
+        tekrardenebtn.setOnClickListener {
+            startActivity(Intent(this@SonucEkraniActivity,MainActivity::class.java))
+
+        }
+
     }
 }
