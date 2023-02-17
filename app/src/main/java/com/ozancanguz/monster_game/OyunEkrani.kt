2be9ca1ba996c8f8781d5b6aee24491cf7a976a7
Ozan <ozancangu24@gmail.com>
2023-02-17 18:35:11 +0300

    -ana karakter serbest hareketi eklendi

--- app/src/main/java/com/ozancanguz/monster_game/OyunEkrani.kt
+++ app/src/main/java/com/ozancanguz/monster_game/OyunEkrani.kt
@@ -3,21 +3,31 @@ package com.ozancanguz.monster_game
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.activity_oyun_ekrani.*
+import java.util.*
+import kotlin.concurrent.schedule
 
 class OyunEkrani : AppCompatActivity() {
+
+    // positions
+    private var anakarakterX=0.0f
+    private var anakarakterY=0.0f
+
+    // controls
+    private var dokunmaControl=true
+
+    private val timer  = Timer()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_oyun_ekrani)
 
-        anakarakter.setOnClickListener {
-            startActivity(Intent(this@OyunEkrani,SonucEkraniActivity::class.java))
-
-            finish()
-        }
 
         cl.setOnTouchListener(object: View.OnTouchListener{
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
@@ -32,6 +42,25 @@ class OyunEkrani : AppCompatActivity() {
                     Log.d("oyunekrani","Down")
 
                 }
+
+                anakarakterX=anakarakter.x
+                anakarakterY=anakarakter.y
+
+
+                timer.schedule(0,20){
+                    Handler(Looper.getMainLooper()).post{
+                        if(dokunmaControl){
+                            anakarakterY-=20.0f
+                        }else{
+                            anakarakterY+=20.0f
+
+                        }
+                        anakarakter.y=anakarakterY
+                    }
+                }
+
+
+
                 return true
             }
 
@@ -40,4 +69,10 @@ class OyunEkrani : AppCompatActivity() {
 
 
     }
+
+
+
+
+
+
 }
