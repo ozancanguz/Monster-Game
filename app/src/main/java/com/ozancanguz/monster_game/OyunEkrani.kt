32b2d682608f789e80bddcde0865d56beed752c4
Ozan <ozancangu24@gmail.com>
2023-02-17 18:18:28 +0300

    -touch property added

--- app/src/main/java/com/ozancanguz/monster_game/OyunEkrani.kt
+++ app/src/main/java/com/ozancanguz/monster_game/OyunEkrani.kt
@@ -3,6 +3,9 @@ package com.ozancanguz.monster_game
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import kotlinx.android.synthetic.main.activity_oyun_ekrani.*
 
 class OyunEkrani : AppCompatActivity() {
@@ -15,6 +18,24 @@ class OyunEkrani : AppCompatActivity() {
 
             finish()
         }
+
+        cl.setOnTouchListener(object: View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+
+
+                if(event?.action==MotionEvent.ACTION_DOWN){
+
+                    Log.d("oyunekrani","Down")
+                }
+                if(event?.action==MotionEvent.ACTION_UP){
+                    Log.d("oyunekrani","Down")
+
+                }
+                return true
+            }
+
+        })
 
 
 
