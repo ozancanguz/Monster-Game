82f69821d710a67014d9b697d1b0edd3734b0f25
Ozan <ozancangu24@gmail.com>
2023-02-17 17:35:02 +0300

    Initial commit

---
+++ app/src/main/java/com/ozancanguz/monster_game/OyunEkrani.kt
@@ -0,0 +1,15 @@
+package com.ozancanguz.monster_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OyunEkrani : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oyun_ekrani)
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/ozancanguz/monster_game/SonucEkraniActivity.kt
@@ -0,0 +1,11 @@
+package com.ozancanguz.monster_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SonucEkraniActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sonuc_ekrani)
+    }
+}
